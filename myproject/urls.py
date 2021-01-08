1a2f2aa31b43808e77ec9d9e97e5f26cedd83a6e
mosttoffa <mostofacba@gmail.com>
2021-01-08 18:14:09 +0600

    Create Django project, Migrations, Createsuperuser and Main App.

---
+++ myproject/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    
+    url(r'^admin/' , admin.site.urls),
+    url(r'', include('main.urls')),
+
+  
+]
