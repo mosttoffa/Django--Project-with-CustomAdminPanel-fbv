d3eb8ac68ecb35ac45c2525bbd426e71d652bd00
mosttoffa <mostofacba@gmail.com>
2021-01-11 05:28:18 +0600

    Authenticate- Lock Admin Page, Make Login & Logout

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.home, name='home'),
+    url(r'^panel/$', views.panel, name='panel'),    
+    url(r'^login/$', views.my_login, name='my_login'),    
+    url(r'^logout/$', views.my_logout, name='my_logout'),    
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^product/$', views.product, name='product'),
     url(r'^product_detail/$', views.product_detail, name='product_detail'),
-    url(r'^panel/$', views.panel, name='panel'),
-  
-  
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Main
-
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -12,11 +12,46 @@ def home(request):
     return render(request, 'front/home.html', {'sitename':sitename})
 
 
+
+def panel(request):
+
+    #Login check start
+    if not request.user.is_authenticated:
+        return redirect('my_login')
+    #Login check end
+
+    return render(request, 'back/home.html')
+
+
+def my_login(request):
+
+    #Login authentication
+    if request.method == 'POST':
+        utxt = request.POST.get('username')
+        ptxt = request.POST.get('password')
+              #user authentic
+        if utxt != "" and ptxt !="":
+            user = authenticate(username=utxt, password=ptxt)
+                #user does't exixt
+            if user != None:
+                login(request,user)
+                return redirect('panel')
+
+    return render(request, 'front/login.html')
+
+def my_logout(request):
+
+    logout(request)
+    
+    return redirect('my_login')
+
+
+
 def contact(request):
 
-    sitename = "Amin Tech Ltd | Contact"
+    # sitename = "Amin Tech Ltd | Contact"
 
-    return render(request, 'front/contact.html', {'sitename':sitename})
+    return render(request, 'front/contact.html')
 
 
 def product(request):
@@ -32,9 +67,4 @@ def product_detail(request):
 
     return render(request, 'front/product_detail.html', {'sitename':sitename})
 
-
-def panel(request):
-
-
-        return render(request, 'back/home.html')
 
