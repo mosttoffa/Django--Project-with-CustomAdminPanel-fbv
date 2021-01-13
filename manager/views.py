32baeefd6e45d53d948f896e62ee610ff82b4608
mosttoffa <mostofacba@gmail.com>
2021-01-14 03:21:20 +0600

    Create Group for Users to give thm access to Panel

--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Manager
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 
 
 # Create your views here.
@@ -26,4 +26,88 @@ def manager_del(request,pk):
     manager.delete()
 
     return redirect('manager_list')
+
+
+def manager_group(request):
+
+    group = Group.objects.all()
+
+    return render(request, 'back/manager_group.html', {'group':group})
+
+
+def manager_group_add(request):
+
+
+    if request.method == 'POST' :
+
+        name = request.POST.get('name')
+
+        if name != "" :
+
+            if len(Group.objects.filter(name=name)) == 0 :
+
+                group = Group(name=name)
+                group.save()
+
+    return redirect('manager_group')
+
+
+
+def manager_group_del(request, name):
+
+
+    b = Group.objects.filter(name=name)
+    b.delete()
+
+
+    return redirect('manager_group')
+
+
+
+def users_groups(request,pk):
+
+    
+    manager = Manager.objects.get(pk=pk)
+
+    user = User.objects.get(username=manager.utxt)
+
+    ugroup = []
+    for i in user.groups.all():
+        ugroup.append(i.name)
+    
+    group = Group.objects.all()
+
+    return render(request, 'back/users_groups.html', {'ugroup':ugroup, 'group':group, 'pk':pk})
+
+
+
+def add_users_to_groups(request,pk):
+
+
+    if request.method == 'POST' :
+
+        gname = request.POST.get('gname')
+
+        group = Group.objects.get(name=gname)
+        manager = Manager.objects.get(pk=pk)
+        user = User.objects.get(username=manager.utxt)
+        user.groups.add(group)
+    
+
+    return redirect('users_groups' , pk=pk)
+
+
+
+def del_users_to_groups(request,pk,name):
+
+
+    group = Group.objects.get(name=name)
+    manager = Manager.objects.get(pk=pk)
+    user = User.objects.get(username=manager.utxt)
+    user.groups.remove(group)
+    
+    
+
+    return redirect('users_groups' , pk=pk)
+
 
